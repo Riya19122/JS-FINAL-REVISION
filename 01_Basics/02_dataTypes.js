47582ac90847039ef260ca1116854c7c6340d0fc
Riya Jain <jriya9313@gmail.com>
2025-08-19 12:00:46 +0530

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // it means treat all the JS code as newer version 
+
+// alert(3 + 3)  // it is not working bcoz we are using node.js not browser
+
+// console.log(3
+//     +3)   // So code readability that should be high
+
+console.log("riya")
+
+let number;  // undefined
+let char = null // null it means here it is empty
+
+// Primitive Datatypes 
+  // 1. number  => 2 to the power of 53 which is range
+  // 2. bigInt  => when the number is very big then it will use like in trading , stocking etc
+  // 3. String  => it is used for the characters so we will always write it in double quots(" ")
+  // 4. boolean => it means only true or false
+  // 5. null => it is standalone value
+  // 6. undefined  => in which when we dont assign any value only declare variable then their type is undefined
+  // 7. Symbol => to find the uniqueness of the variable  ,many figma tools uses symbol 
+
+// Objects
+  
+
+console.log(typeof "riya")
+console.log(typeof null);  //here null is object
+console.log(typeof undefined);  // here it is type in javascript
+
+
+
+
