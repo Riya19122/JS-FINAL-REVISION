3ad4ac59318fdc6cc90cc4c00dcc24ad44c6e8e3
Riya Jain <jriya9313@gmail.com>
2025-08-20 15:20:31 +0530

    conversions in datatypes

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(2 >= 1)
+// console.log(1 == 2)
+// console.log(2 <= 1)
+// console.log(2 < 1)
+// console.log(2 != 1)
+
+// comparisons in different datatypes but we will not use these complicated conversions
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null >= 0)  // it gives true bcoz greater than or smaller than first convert null into zero than compare
+// console.log(null < 0)
+// console.log(null == 0)
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// === it compares datatypes also 
+console.log("2" === 2);
+
+
+
+
+
