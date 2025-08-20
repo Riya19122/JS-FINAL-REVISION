7c2cd7f2f6759a789b2f9d8a4827999e7d589a37
Riya Jain <jriya9313@gmail.com>
2025-08-20 14:35:23 +0530

    Conversion of Datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,58 @@
+// // conversion in number 
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNum = Number(score)  // type conversion from string to number
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+/*
+type conversions in numbers
+1. "33" => easily convert it in number which is 33
+2. "33abc" => it will also convert in number by gives output is NaN
+3. null => it converts and give output is 0
+4. undefined => it also gives the NaN
+5. "true" / "false" => it also converts and give 1 for true and 0 for false
+*/
+
+// // conversion in Boolean
+// let isLoggedIn = 1;
+// console.log(typeof isLoggedIn)
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+Type Conversion in Boolean
+1. 1 => true 
+2. 0 => false 
+3. " " => false 
+4. "riya" => true
+*/
+
+// conversion in string
+// let someNumber = undefined
+// console.log(typeof someNumber);
+
+// let valueInString = String(someNumber)
+// console.log(typeof valueInString);
+// console.log(valueInString);
+
+/*
+type conversion in string
+1. 33 => converted in string and give output 33
+2. true/false => converted in string and gives output true/false
+3. null => converted in string and gives output null
+4. undefined => converted in string and gives outpur undefined
+*/
+
+
+
+
+
+
+
+
+
