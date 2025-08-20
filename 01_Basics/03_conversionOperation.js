2be6cc720c67ba88af74c59820eceb3b8bb4920f
Riya Jain <jriya9313@gmail.com>
2025-08-20 15:01:41 +0530

    conversion of datatypes and operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -49,6 +49,59 @@ type conversion in string
 */
 
 
+//  *************************************  OPERATIONS  ********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+  // Common Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " riya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+// console.log(+false)
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter);
+
+// link to more study 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
