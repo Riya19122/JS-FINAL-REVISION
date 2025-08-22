d560ca516a5cc2f7c8174dd297a0dc2ba24dcde2
Riya Jain <jriya9313@gmail.com>
2025-08-22 15:17:51 +0530

    Array Part 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,68 @@
+// Arrays
+   // Array declaration
+const myArr = [0,1,2,3,4,5,6]
+// const heroes = ["shaktiman" , "naagraj"]
+// const myArr2 = new Array(1 ,2 ,3 , 4,5 ,6)
+
+// console.log(myArr[0]);   //find out the element at the index given
+// console.log(myArr2);
+
+//  Arrays Method
+    // 1. push method  => it adds the element to the last index of the array
+    // myArr.push(7)
+    // console.log(myArr)
+
+    // 2. pop method   => it removes the element from the last index of an array
+    // myArr.pop();
+    // console.log(myArr);
+
+    // 3. unshift method => it adds the element to the first index and all values shifted by 1 index
+    // myArr.unshift(9)
+    // console.log(myArr);
+
+    // 4. shift method => it removes element from the first index 
+    // myArr.shift()
+    // console.log(myArr);
+
+    // 5. includes method => it gives boolean output like if that element is present then give true otherwise false
+    // console.log(myArr.includes(7))
+    // console.log(myArr.includes(5))
+
+    // 6. indexOf method => it tells about the element which present on that index
+    // console.log(myArr.indexOf(2));
+    // console.log(myArr.indexOf(10));  //gives -1 bcoz element is not present at tht index
+    
+    // 7. Join method => it gives the same array to the string (type conversion)
+    // const newArr = myArr.join()
+    // console.log(newArr)
+    // console.log(typeof newArr);
+
+    // 8. Slice method => it gives part of array without modifying the array 
+    // console.log("A" , myArr)
+    // console.log(myArr.slice(1,3))
+
+    // 9. Splice method => it deletes the part of array with modifying the array
+    // console.log("B" , myArr)
+    // console.log(myArr.splice(1,3));
+
+    // console.log("C" ,myArr) // here after splice it gives only modifying array
+
+    
+
+    
+    
+
+
+
+    
+    
+
+    
+    
+    
+
+
+    
+
+
+
