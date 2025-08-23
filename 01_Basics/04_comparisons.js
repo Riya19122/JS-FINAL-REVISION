3296685c6d35c2a6a7ed3e0f1bb2a7dc82853fea
Riya Jain <jriya9313@gmail.com>
2025-08-23 18:02:00 +0530

    comparisons in js

--- 01_Basics/04_comparisons.js
+++ 01_Basics/04_comparisons.js
@@ -6,7 +6,7 @@
 // console.log(2 != 1)
 
 // comparisons in different datatypes but we will not use these complicated conversions
-// console.log("2" > 1)
+// console.log("2" > 1)    //gives true bcoz it converts in number then compare it
 // console.log("02" > 1)
 
 // console.log(null >= 0)  // it gives true bcoz greater than or smaller than first convert null into zero than compare
